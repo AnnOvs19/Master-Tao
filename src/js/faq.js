54eeb8734dcb034f87a21f0b42faae153433a994
AnnOvs19 <annaovsyanikowa@yandex.ru>
2023-12-06 13:56:57 +0300

    валидация faq-form

--- src/js/faq.js
+++ src/js/faq.js
@@ -18,3 +18,48 @@ function clearClassList() {
         item.classList.remove("open");
     })
 }
+
+//Валидация и отправка формы
+
+const faq_form = document.querySelector(".faq-form");
+const faq_form_inputs = faq_form.querySelectorAll("input")
+const faq_form_textarea = document.querySelector(".faq-form__ta");
+const faq_submit = document.querySelector(".faq-submit");
+const valid = [];
+let status_form = false;
+
+
+faq_submit.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    faq_form_inputs.forEach((elem) => {
+        if (elem.value != "") {
+            status_form = true;
+            valid.push(status_form)
+        } else {
+            status_form = false;
+            valid.push(status_form)
+        }
+    });
+
+    valid.forEach((elem) => {
+        if (elem === false) {
+            status_form = false;
+        }
+    });
+
+    if (status_form === true) {
+        const form_data = new FormData(faq_form);
+        fetch("/pups", {
+            method: "POST",
+            body: form_data
+        });
+
+        faq_form_inputs.forEach((elem) => {
+            elem.value = "";
+        })
+
+        faq_form_textarea.value = "";
+    }
+
+})
